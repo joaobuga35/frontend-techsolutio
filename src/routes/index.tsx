61510fc71b528754b1a6f966013fcb447aea6a7e
joaobuga35 <joaobuga35@gmail.com>
2023-09-05 10:43:43 -0300

    feat: add dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import ProtectedRoutes from "../components/ProtectedRoutes";
 
 const RoutesMain = () => {
   return (
     <Routes>
-      <Route path="/" element={<h1>Login</h1>}></Route>
-      <Route path="/register" element={<h1>Register</h1>}></Route>
-      <Route element={<h1>Dashboard</h1>}></Route>
+      <Route path="/" element={<Login />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+      </Route>
     </Routes>
   );
 };
